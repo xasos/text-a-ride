0312b407fd8d2cc456d79367ff6f4d9d0e202957
Niraj Pant <nirajpant7@gmail.com>
2015-09-12 17:30:48 -0400

    Add Twilio Methods

--- sms-quickstart.rb
+++ sms-quickstart.rb
@@ -4,8 +4,13 @@ require 'sinatra'
 require 'pry'
 
 get '/sms-quickstart' do
+  sender = params[:From]
+  friends = {
+   "+12243667047" => "Niraj"
+  }
+  name = friends[sender] || "Mobile Monkey"
   twiml = Twilio::TwiML::Response.new do |r|
-    r.message "Message"
+    r.message "Hello, #{name}. Thanks for the message."
   end
   twiml.text
 end
